12541722fa212cc234a3e2e583ae81b10b8b8751
shonenenenene <danya.sytaev@gmail.com>
2022-07-09 18:45:47 +0300

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Main from "./main/Main"
 
 
@@ -12,11 +12,11 @@ const dispatch = useDispatch()
 
     return (
         
-        <BrowserRouter>
+        <HashRouter>
             <Routes >
                     <Route path="/" element={<Main />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         
     )
 }
